55dcce9ec29e81734c37a67fb20aa772e3addb2b
yishengpei <73866013+yishengpei@users.noreply.github.com>
2024-03-15 21:02:04 +0000

    leetcode5 longest-palindromic-substring

---
+++ 5.longest-palindromic-substring.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=5 lang=python3
+#
+# [5] Longest Palindromic Substring
+#
+
+# @lc code=start
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        res = ""
+        for i in range(len(s)):
+            # 以 s[i] 为中心的最长回文子串
+            s1 = self.palindrome(s, i, i)
+            # 以 s[i] 和 s[i+1] 为中心的最长回文子串
+            s2 = self.palindrome(s, i, i + 1)
+            # res = longest(res, s1, s2)
+            res = res if len(res) > len(s1) else s1
+            res = res if len(res) > len(s2) else s2
+        return res
+
+    def palindrome(self, s, l, r):
+        while (l >= 0 and r < len(s) and s[l] == s[r]):
+            l -= 1
+            r += 1
+        return s[l+1:r]
+# @lc code=end
+
