161a9d3ad62696658853f3ef4121e8bc862cee80
yishengpei <73866013+yishengpei@users.noreply.github.com>
2024-05-30 11:17:35 +0100

    leetcode404 sum-of-left-leaves

---
+++ 404.sum-of-left-leaves.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode id=404 lang=python3
+#
+# [404] Sum of Left Leaves
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        elif root.left and not root.left.left and not root.left.right:
+            return root.left.val + self.sumOfLeftLeaves(root.right)
+        else:
+            return self.sumOfLeftLeaves(root.left) + self.sumOfLeftLeaves(root.right)
+
+# @lc code=end
+
