37452f9fa4982587b1d930ad9457f0f1dffe87e1
yishengpei <73866013+yishengpei@users.noreply.github.com>
2024-06-12 09:08:12 +0100

    leetcode75 sort-colors

---
+++ 75.sort-colors.py
@@ -0,0 +1,21 @@
+from typing import List
+#
+# @lc app=leetcode id=75 lang=python3
+#
+# [75] Sort Colors
+#
+
+# @lc code=start
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        for i in range(n-1):
+            for j in range(i+1,n):
+                if nums[i] > nums[j]:
+                    nums[i],nums[j] = nums[j],nums[i]
+                    
+# @lc code=end
+
